54b97feaa9c107d6141aeb97e35deeaf27a1e4f2
ryand67 <rwdevenney@gmail.com>
2021-01-28 19:46:14 -0500

    required forms

--- pages/submit.js
+++ pages/submit.js
@@ -41,13 +41,13 @@ export default function submit() {
             </Head>
 
             <main className='box-border m-0 p-0 flex justify-center items-center w-100 h-screen'>
-                <form onSubmit={handleFormSubmit} className="shadow border-2 border-transparent bg-gray-100 w-1/3 h-2/3 py-8 flex justify-center items-center flex-col rounded-xl" id="submitForm">
+                <form onSubmit={handleFormSubmit} className="shadow border-2 border-transparent bg-gray-100 w-2/3 h-2/3 py-8 flex justify-center items-center flex-col rounded-xl l:w-2/3" id="submitForm">
                     <label className="text-lg py-2" htmlFor="name">Name:</label>
-                    <input onChange={handleNameChange} className="px-4 py-2 border focus:ring-gray-500 focus:border-gray-900 w-2/3 sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600" type="text" name="name" placeholder="Name"/>
+                    <input required onChange={handleNameChange} className="px-4 py-2 border focus:ring-gray-500 focus:border-gray-900 w-2/3 sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600" type="text" name="name" placeholder="Name"/>
                     <label className="text-lg py-2" htmlFor="subject">Subject:</label>
-                    <input onChange={handleSubjectChange} className="px-4 py-2 border focus:ring-gray-500 focus:border-gray-900 w-2/3 sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600" type="text" name="subject" placeholder="Subject"/>
+                    <input required onChange={handleSubjectChange} className="px-4 py-2 border focus:ring-gray-500 focus:border-gray-900 w-2/3 sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600" type="text" name="subject" placeholder="Subject"/>
                     <label className="text-lg py-2" htmlFor="bug">Issue Description:</label>
-                    <textarea onChange={handleIssueChange} className="px-4 py-2 border h-1/2 focus:ring-gray-500 focus:border-gray-900 w-2/3 sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600" name="bug" id="" cols="30" rows="10" placeholder="What's going wrong?"></textarea>
+                    <textarea required onChange={handleIssueChange} className="px-4 py-2 border h-1/2 focus:ring-gray-500 focus:border-gray-900 w-2/3 sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600" name="bug" id="" cols="30" rows="10" placeholder="What's going wrong?"></textarea>
                     <p href="" className="w-1/4 text-center border-3 bg-red-400 cursor-pointer text-white my-8 p-2 rounded-xl transition hover:bg-red-500" onClick={handleFormSubmit}>Submit</p>
                </form>
             </main>
