04af7803a2dd78b613d6d473587b0313bfc4a3ba
ryand67 <rwdevenney@gmail.com>
2021-02-04 16:36:12 -0500

    nav styling

--- components/Nav.js
+++ components/Nav.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 export default function Nav() {
     return (
-        <div className="flex flex-wrap py-2">
-            <div className="w-full px-4">
-                <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg bg-gray-500 rounded">
+        <div className="flex flex-wrap">
+            <div className="w-full">
+                <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg bg-gray-500 rounded-b-m">
                     <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
                         <div className="w-full relative flex justify-between lg:w-auto px-4 lg:static lg:block lg:justify-start">
                             <a className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-no-wrap uppercase text-white" href="/">
